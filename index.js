16a91b5f0360a2221e32421b882d4d9f393c8abf
kapil-khursade <kapilkhursade.210@gmail.com>
2022-10-13 13:28:49 +0530

    first dra

---
+++ index.js
@@ -0,0 +1,8 @@
+function visName(n){
+    document.querySelector(`#logoName>div:nth-child(${n})`).style.visibility = "visible";
+}
+
+function hidName(n){
+    document.querySelector(`#logoName>div:nth-child(${n})`).style.visibility = "hidden";
+}
+
