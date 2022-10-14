94d6859300e560c30e21057adbece1e729733ef3
bob <bob@>
2022-10-15 00:03:06 +0530

    third

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function hidName(n){
     document.querySelector(`#logoName>div:nth-child(${n})`).style.visibility = "hidden";
 }
 
+function myFunction(x) {
+    x.classList.toggle("change");  
+    x.children[3].classList.toggle("mobBarVis");
+}
+
+
