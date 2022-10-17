7e8cf336b879fc936704376908ab2496ded7766d
bob <bob@>
2022-10-17 19:09:09 +0530

    fouth

--- index.js
+++ index.js
@@ -11,4 +11,19 @@ function myFunction(x) {
     x.children[3].classList.toggle("mobBarVis");
 }
 
+// Theme change
+let theme = ["rgb(102,3,47)", "rgb(104,126,169)", "rgb(251,87,70)","rgb(0,188,34)", "rgb(34, 32, 32)"];
+let themeName = ["Thanos", "Aladin", "Garfiled", "Hulk", "Jhon Wick"];
 
+let i = Number(localStorage.getItem("themeI"))||theme.length-1;
+
+function changeTheme(x){
+   document.querySelector("body").style.backgroundColor=theme[i];
+   document.querySelector("#navBar").style.backgroundColor=theme[i];
+   x.innerText =themeName[i];
+   i++;
+   if(i>=theme.length){
+    i=0;
+   }
+   localStorage.setItem("themeI", i);
+}
