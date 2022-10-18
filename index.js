206aaf7184ad4d26fac20ccc51fe56595a6da75a
bob <bob@>
2022-10-18 21:47:17 +0530

    kkdjsck

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+// Visibility of the  logo name
 function visName(n){
     document.querySelector(`#logoName>div:nth-child(${n})`).style.visibility = "visible";
 }
-
 function hidName(n){
     document.querySelector(`#logoName>div:nth-child(${n})`).style.visibility = "hidden";
 }
 
+// This is mobile navbar option
 function myFunction(x) {
     x.classList.toggle("change");  
     x.children[3].classList.toggle("mobBarVis");
 }
 
 // Theme change
-let theme = ["rgb(102,3,47)", "rgb(104,126,169)", "rgb(251,87,70)","rgb(0,188,34)", "rgb(34, 32, 32)"];
-let themeName = ["Thanos", "Aladin", "Garfiled", "Hulk", "Jhon Wick"];
+let theme = ["rgb(69,38,47)", "rgb(104,126,169)", "rgb(40,51,45)", "rgb(252,100,0)","rgb(129,170,29)", "rgb(93,50,23)", "rgb(34, 32, 32)"];
+let themeName = ["Thanos", "Aladdin", "Gabbar Shing", "Garfiled", "Hulk", "Shingham", "Jhon Wick"];
 
-let i = Number(localStorage.getItem("themeI"))||theme.length-1;
+let i = 0;
 
 function changeTheme(x){
    document.querySelector("body").style.backgroundColor=theme[i];
@@ -27,3 +28,5 @@ function changeTheme(x){
    }
    localStorage.setItem("themeI", i);
 }
+
+// 
