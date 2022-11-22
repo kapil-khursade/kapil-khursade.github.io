cb8b08e0c44807fae8fc1769678f3a0f5fca6764
bob <bob@>
2022-11-22 12:14:33 +0530

    navbarfix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let i = 0;
 
 function changeTheme(x){
    document.querySelector("body").style.backgroundColor=theme[i];
-   document.querySelector("#navBar").style.backgroundColor=theme[i];
+   document.querySelector("#nav").style.backgroundColor=theme[i];
    x.innerText =themeName[i];
    i++;
    if(i>=theme.length){
@@ -29,5 +29,36 @@ function changeTheme(x){
    localStorage.setItem("themeI", i);
 }
 
-// Current
+// Navbar code
+var util = {
 
+    scrollEvent() {
+      var scrollPosition = $(document).scrollTop();
+  
+      $.each(util.scrollMenuIds, function (i) {
+        var link = util.scrollMenuIds[i],
+          container = $(link).attr("href"),
+          containerOffset = $(container).offset().top,
+          containerHeight = $(container).outerHeight(),
+          containerBottom = containerOffset + containerHeight;
+  
+        if (
+          scrollPosition < containerBottom-120
+           &&
+          scrollPosition >= containerOffset-120
+        ) {
+          $(link).addClass("active");
+        } else {
+          $(link).removeClass("active");
+        }
+      });
+    }
+  };
+  
+  $(document).ready(function () {
+    util.scrollMenuIds = $("a.nav-link[href]");
+    $("#menu").click(util.mobileMenu);
+    $(window).resize(util.windowResize);
+    $(document).scroll(util.scrollEvent);
+  });
+  
