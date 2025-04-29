db2f03cf0cea153df50579ba554c71ad87545874
kapil-khursade <kapilkhursade.210@gmail.com>
2025-04-29 10:19:25 +0530

    added emal js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
@@ -7,24 +8,48 @@ import Button from 'react-bootstrap/Button';
 import { Badge } from 'react-bootstrap';
 
 function Contact() {
+
+  const form = useRef();
+  const [messageSent, setMessageSent] = useState('FORM')
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_bipup5k', 'template_l4dpncb', form.current, {
+        publicKey: 'p_LdBJU-8bQGzT8rz',
+      })
+      .then(
+        () => {
+          setMessageSent('SUCCESS')
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setMessageSent('ERROR')
+        },
+      );
+  };
+
   return (
     <Container
         id="contact_me"
         className="d-flex justify-content-end align-items-center vh-100"
     >
-      <Card className='w-100'>
+      {messageSent=='FORM' && <Card className='w-100'>
         <Card.Header className='text-center'>
           Drop Your Message!
-          <Badge bg="danger" className="ms-3">Out Of Order</Badge>
+          {
+          //<Badge bg="danger" className="ms-3">Out Of Order</Badge>
+          }
         </Card.Header>
         <Card.Body>
-          <Form>
+          <Form  ref={form} onSubmit={sendEmail}>
             <FloatingLabel
               controlId="floatingInput"
               label="Your Email address"
               className="mb-3"
             >
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control type="email" placeholder="name@example.com" name="user_email" />
             </FloatingLabel>
 
             <FloatingLabel
@@ -32,15 +57,7 @@ function Contact() {
               label="Your Name"
               className="mb-3"
             >
-              <Form.Control type="text" placeholder="John Doe" />
-            </FloatingLabel>
-
-            <FloatingLabel
-              controlId="floatingOrgName"
-              label="Your Organization Name"
-              className="mb-3"
-            >
-              <Form.Control type="text" placeholder="ABC Pvt Ltd" />
+              <Form.Control type="text" placeholder="John Doe" name="user_name"/>
             </FloatingLabel>
 
             <FloatingLabel
@@ -52,12 +69,27 @@ function Contact() {
                 as="textarea"
                 placeholder="Leave a message here"
                 style={{ height: '100px' }}
+                name="message"
               />
             </FloatingLabel>
-            <div className='d-flex justify-content-end'><Button variant="primary" type="submit" disabled={true}>Send</Button></div>
+            <div className='d-flex justify-content-end'><Button variant="primary" type="submit">Send</Button></div>
           </Form>
         </Card.Body>
-      </Card>
+      </Card>}
+
+      {messageSent=='SUCCESS' && <Card className='w-100'>
+        <Card.Header className='text-center'>
+          Thanks for reaching out to me. I will get back to you soon.
+        </Card.Header>
+        </Card>
+      }
+      {messageSent=='ERROR' && <Card className='w-100'>
+        <Card.Header className='text-center'>
+          Sorry.
+          <Badge bg="danger" className="ms-3">Something Went Wrong.</Badge>
+        </Card.Header>
+        </Card>
+      }
     </Container>
   );
 }
